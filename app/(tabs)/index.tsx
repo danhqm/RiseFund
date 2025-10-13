2f5ad9cf0584a2787dd6acd1c8a8cf215985fbac
ahemie <ahakiemie@gmail.com>
2025-10-14 03:04:50 +0800

    first

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello</ThemedText>
+        <ThemedText type="title">Kieeiaeiaieaieia</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
